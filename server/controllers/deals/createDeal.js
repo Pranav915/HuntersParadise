d6ed2d4c29b5fc8c087c7d5ae1b05de7a979bdae
Rahul Raut <rkr2137@gmail.com>
2023-10-18 04:11:06 +0530

    removed the bug from createDeal

--- server/controllers/deals/createDeal.js
+++ server/controllers/deals/createDeal.js
@@ -2,16 +2,17 @@ const LiveDeals = require('../../models/LiveDeals');
 const createDeal = (req, res) => {
     if(req.headers.from != "Pranav"){
         res.status(401).send("Not authorized");
+        return;
     }
     const data = (req.body.messages[0]).data;
     const newDeal = new LiveDeals({
-        productName: data.get("productName"),
-        productImage: data.get("productImage"),
-        category: data.get("category"),
-        dealDescription: data.get("dealDescription"),
-        askPrice: data.get("askPrice"),
-        seller: data.get(seller),
-        sellerName: data.get("sellerName"),
+        productName: data.productName,
+        productImage: data.productImage,
+        category: data.category,
+        dealDescription: data.dealDescription,
+        askPrice: data.askPrice,
+        seller: data.seller,
+        sellerName: data.sellerName,
     });
     newDeal.save()
     .then((deal) => {
