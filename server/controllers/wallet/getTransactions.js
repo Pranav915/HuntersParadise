8c8773befd744aeff84196860b0b01ff5c2a027d
Rahul Raut <rkr2137@gmail.com>
2023-11-01 12:31:06 +0530

    get my getTransactions

--- server/controllers/wallet/getTransactions.js
+++ server/controllers/wallet/getTransactions.js
@@ -1,10 +1,26 @@
 const User = require('../../models/User');
 const getTransactions = (req, res) => {
-  User.findOne({ _id: req.user.userId })
+  try{
+    User.findOne({ _id: req.user.userId })
     .populate("wallet.transactions")
     .then((user) => {
-    console.log(user);
-      res.status(200).send(user.wallet);
+      var completeTransaction = [];
+      var pendingTransaction = [];
+      user.wallet.transactions.forEach((trns) => {
+        if(trns.status == "pending"){
+          pendingTransaction.push(trns);
+        }else{
+          completeTransaction.push(trns);
+        }
+      });
+      res.status(200).send({
+        pendingTransaction: pendingTransaction,
+        completeTransaction: completeTransaction
+      });
     });
+  }catch(err){
+    console.log("Error while fetching transactions", err);
+    res.status(501).send("Internal Server Error");
+  }
 };
 module.exports = getTransactions;
